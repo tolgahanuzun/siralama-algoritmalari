d52a72160a1fa48b42f1fdeaa0dddb01eb3c50be
tolgahanuzun <mail@tolgahanuzun.com>
2015-10-28 15:16:29 +0300

    new Sorting header file

---
+++ Sorting.h
@@ -0,0 +1,23 @@
+#ifndef _SORTİNG_
+#define _SORTİNG_
+
+void bubbleSort(int dizi[], int elemanSayisi)
+{
+    int temp;
+    int i, j;
+
+    for (i=1; i<elemanSayisi; i++)      
+    {
+        for (j=0; j<elemanSayisi-i; j++)   
+        {
+            if(dizi[j] > dizi[j+1])
+            {
+                temp = dizi [j];
+                dizi [j] = dizi [j+1];
+                dizi [j+1] = temp;                     
+             }
+        }
+    }
+}
+
+#endif
